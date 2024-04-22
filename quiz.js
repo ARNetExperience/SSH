09bdf4083ae472fafcf4d078b70e85b7fafa7bec
ARNetExperience <165185682+ARNetExperience@users.noreply.github.com>
2024-04-22 20:18:47 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,109 @@
+//Question bank
+var questionBank= [
+    {
+        "question": "What does SSH stand for?",
+        "option": ["Secure Shell", "Secure Socket Host", "Simple Secure Hosting", "System Security Helper"],
+        "answer": "Secure Shell"
+    },
+    {
+        "question": "Which layer of the OSI model does SSH primarily operate on?",
+        "option": ["Application layer", "Transport layer", "Network layer", "Data link layer"],
+        "answer": "Application layer"
+    },
+    {
+        "question": "What is the primary purpose of SSH?",
+        "option": ["To transfer files between computers", "To establish secure connections between devices", "To manage network congestion", "To resolve domain names to IP addresses"],
+        "answer": "To establish secure connections between devices"
+    },
+    {
+        "question": "Which protocol does SSH primarily use for encryption and authentication?",
+        "option": ["HTTP", "FTP", "TCP", "SSH protocol"],
+        "answer": "SSH protocol"
+    },
+    {
+        "question": "What is the default port number for SSH?",
+        "option": ["20", "21", "22", "23"],
+        "answer": "22"
+    }
+    
+    
+    
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var option2= document.getElementById('option2');
+var option3= document.getElementById('option3');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var score= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    option2.innerHTML= questionBank[i].option[2];
+    option3.innerHTML= questionBank[i].option[3];
+    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+}
+
+//function to calculate scores
+function calcScore(e){
+    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
+    {
+        score= score+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        points.innerHTML= score+ '/'+ questionBank.length;
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+//function to check Answers
+function checkAnswer(){
+    var answerBank= document.getElementById('answerBank');
+    var answers= document.getElementById('answers');
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    for(var a=0;a<questionBank.length;a++)
+    {
+        var list= document.createElement('li');
+        list.innerHTML= questionBank[a].answer;
+        answers.appendChild(list);
+    }
+}
+
+
+displayQuestion();
